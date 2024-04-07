0e393cd009ce68a9f04289f817497dbb35f52f93
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-07 16:28:48 -0500

    setting up the ENV- players

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,8 +17,14 @@ export const Experience = () => {
     start();
     // ** Players handler
     onPlayerJoin((state) => {
+      //------------
+      // The JOYSTICK, it replaces the arrows or the keys to move the player
+      //------------
+      //
       // this will handle the player that is joining
-      // Joystick will only create UI for current player (myPlayer).
+      // *
+      // Joystick will only create UI for current player (myPlayer). (We need to create a joystick for each player. Its sync through the network so each client will have access to the joystick of all the other, but it will only create an UI for the current player)
+      // *
       // For others, it will only sync their state
 
       const joystick = new Joystick(state, {
