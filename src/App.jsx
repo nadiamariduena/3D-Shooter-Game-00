a65915202444c7b72e314ffaca9e12bbec75bc86
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-07 13:55:56 -0500

    default assets and env

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    // Position the camera on the TOP
+    <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
