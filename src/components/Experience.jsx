ad63510de86c5579c53e776c0b71da0ab6b689e9
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-07 13:44:33 -0500

    default assets and env

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
+import { Map } from "./Map";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Map />
     </>
   );
 };
