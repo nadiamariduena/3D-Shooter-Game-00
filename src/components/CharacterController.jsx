3d7033c385a7cc2a78c043f5e9c5aedf8c741917
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-08 23:51:21 -0500

    RigidBody

--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -1,3 +1,4 @@
+import { useFrame } from "@react-three/fiber";
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import { useRef, useState } from "react";
 import { CharacterSoldier } from "./CharacterSoldier";
@@ -15,12 +16,32 @@ export const CharacterController = ({
   const group = useRef();
   const character = useRef();
   //
+  // ** to move the character
+  const rigidbody = useRef();
   const [animation, setAnimation] = useState("Idle");
+  //
+  //
+  useFrame(() => {
+    // update player position
+    const angle = joystick.angle();
+    if (joystick.isJoystickPressed() && angle) {
+      setAnimation("Run");
+      character.current.rotation.y = angle;
+      //
+      // move character in its own direction
+      const impulse = {
+        x: Math.sin(angle) * MOVEMENT_SPEED * delta,
+        y: 0,
+        z: Math.cos(angle) * MOVEMENT_SPEED * delta,
+      };
+    }
+  });
+
   //
   //
   return (
     <group ref={group} {...props}>
-      <RigidBody colliders={false}>
+      <RigidBody ref={rigidbody} colliders={false}>
         <group ref={character}>
           <CharacterSoldier
             // bring the props from the CharacterSoldier.jsx line 40
