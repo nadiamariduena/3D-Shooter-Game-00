2820f984a0013c804c27330bec0968903b20c16c
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-11 12:15:42 -0500

    shooting character

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -85,6 +85,9 @@ export const Experience = () => {
           state={state}
           joystick={joystick}
           userPlayer={state.id === myPlayer()?.id}
+          //
+          //
+          onFire={onFire}
         />
       ))}
 
