0b7d8493b4914f419abaa2d5bd2919c9e85f9912
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-12 00:21:50 -0500

    shooting character

--- src/components/Bullet.jsx
+++ src/components/Bullet.jsx
@@ -22,6 +22,18 @@ export const Bullet = ({
   //
   //18
   const rigidbody = useRef();
+  // 22
+  //  We will apply the movement of our bullet
+  // in other words the logic of the bullet has to match the angle of the char
+  useEffect(() => {
+    //
+    const velocity = {
+      x: Math.sin(angle) * BULLET_SPEED,
+      // no shoot to the top, but you can change it if you want, just keep in mind you have to change it in line 96 on the CharacterController.jsx
+      y: 0,
+      z: Math.cos(angle) * BULLET_SPEED,
+    };
+  }, []);
 
   //
   //
@@ -35,6 +47,7 @@ export const Bullet = ({
         //   20
         ref={rigidbody}
       >
+        //21
         <mesh position-z={0.25} material={bulletMaterial} castShadow>
           <boxGeometry args={[0.05, 0.05, 0.5]}></boxGeometry>
         </mesh>
