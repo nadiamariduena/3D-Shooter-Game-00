39a9b69e3ed5755c936727e1a3de5d26d2cc0384
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-24 23:25:21 -0500

    shooting another player

--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -1,8 +1,8 @@
 import { CameraControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { CapsuleCollider, RigidBody, vec3 } from "@react-three/rapier";
 import { isHost } from "playroomkit";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { CharacterSoldier } from "./CharacterSoldier";
 
 //
