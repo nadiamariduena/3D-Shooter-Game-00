cf9114aab362fcc7817762c3759608ac64255e5f
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-26 17:29:04 -0500

    impact effect

--- src/components/BulletHit.jsx
+++ src/components/BulletHit.jsx
@@ -3,3 +3,9 @@ import { useFrame } from "@react-three/fiber";
 import { isHost } from "playroomkit";
 import { useEffect, useMemo, useRef } from "react";
 import { Color, MathUtils, Vector3 } from "three";
+
+//----color blo----
+const bulletHitcolor = new Color("red");
+bulletHitcolor.multiplyScalar(12);
+
+//--------
