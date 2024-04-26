04a47f90586c0ac1f53a5810fb6809e7ceb979a4
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-25 19:43:48 -0500

    shooting another player

--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -287,14 +287,12 @@ const PlayerInfo = ({ state }) => {
         {name}
         <meshBasicMaterial color={state.profile.color} />
       </Text>
-      {/*  */}
       <mesh position-z={-0.1}>
         <planeGeometry args={[1, 0.2]} />
         <meshBasicMaterial color="black" transparent opacity={0.5} />
       </mesh>
-      {/*  */}
       <mesh scale-x={health / 100} position-x={-0.5 * (1 - health / 100)}>
-        <planeGeometry args={[1, 0]} />
+        <planeGeometry args={[1, 0.2]} />
         <meshBasicMaterial color="red" />
       </mesh>
     </Billboard>
