e76c05eb940ff63d1941d0bb62d11fc437c6972a
nadiamariduena <nadia.mm.mariduena@gmail.com>
2024-04-26 17:31:39 -0500

    impact effect

--- src/components/BulletHit.jsx
+++ src/components/BulletHit.jsx
@@ -9,3 +9,44 @@ const bulletHitcolor = new Color("red");
 bulletHitcolor.multiplyScalar(12);
 
 //--------
+
+//
+//
+//
+export const BulletHit = ({ nb = 100, position, onEnded }) => {
+  //  Generate a number of small boxes with a random TARGET position
+  //
+  const boxes = useMemo(
+    () =>
+      Array.from({ length: nb }, () => ({
+        target: new Vector3(
+          MathUtils.randFloat(-0.6, 0.6),
+          MathUtils.randFloat(-0.6, 0.6),
+          MathUtils.randFloat(-0.6, 0.6)
+        ),
+        scale: 0.1, //MathUtils.randFloat(0.03, 0.09),
+        speed: MathUtils.randFloat(0.1, 0.3),
+      })),
+    [nb]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isHost()) {
+        onEnded();
+      }
+    }, 500);
+  }, []);
+
+  return (
+    <group position={[position.x, position.y, position.z]}>
+      <Instances>
+        <boxGeometry />
+        <meshStandardMaterial toneMapped={false} color={bulletHitcolor} />
+        {boxes.map((box, i) => (
+          <AnimatedBox key={i} {...box} />
+        ))}
+      </Instances>
+    </group>
+  );
+};
